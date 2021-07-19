3a35d2a70f1e0ff5e0b0100305b9359dafe0b6a4
Alec Greenaway <aag3@pdx.edu>
2021-07-18 23:33:06 -0700

    Added working boilerplate code form class example.

--- function.js
+++ function.js
@@ -1,23 +1,118 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+// window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+// const recognition = new SpeechRecognition();
+// recognition.continuous = true;
+// recognition.lang = 'en-US';
+// recognition.interimResults = true;
 
-var formOptions = ['departure', 'destination', 'carry on'];
-var grammar = '#JSGF V1.0; grammar formOptions; public <formOptions> = ' + formOptions.join(' | ') + ' ;'
-var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
+// // const synthesis = new speechSynthesis();
+// let speechArea = document.getElementById('field3');
 
-speechRecognitionList.addFromString(grammar, 1);
-recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
-recognition.lang = 'en-US';
+// let recording = false;
+// let startRecognition = (event) => {
+// 	console.log("Speech Recognition Started")
+// 	recognition.continuous = true
+
+// 	recognition.addEventListener('result', event => {
+// 		const transcript = Array.from(event.results)
+// 		.map((result) => result[0])
+// 		.map((result) => result.transcript)
+// 		.join("");
+// 	console.log(transcript)
+// 	speechArea.innerHTML = transcript;
+// 	});
+// 	recording = true;
+// 	recognition.start();
+// }
+// function handleStartRecognition(event) {
+// 	console.log('start speech recognition');
+      
+// 	recognition.addEventListener('error', (event) => {
+// 	  console.log('an error occurred');
+// 	});
+      
+// 	recognition.addEventListener('result', (event) => {
+// 	  const results = Array.from(event.results)
+// 	    .map((item) => item[0].transcript)
+// 	    .join('');
+      
+// 	  console.log(results);
+// 	  transcript.textContent = results;
+// 	});
+      
+// 	recognition.start();
+//       }
+// const stopRecognition = (event) => {
+// 	recognition.stop();
+// 	recording = false;
+
+// }
+
+// document.getElementById('subBtn').addEventListener("click", handleStartRecognition)
+
+
+// // recognition.onresult = function(event) {
+// // 	var result = event.results[0][0].transcript;
+// // 	console.log("result recieved: ", result)
+// // 	console.log('Confidence: ' + event.results[0][0].confidence);
+// //       }
+
+// Create new instance - speech synthesis
+const synthesis = window.speechSynthesis;
+
+// Establish that browser has speech recognition
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+// Create new instance - speech recognition
+const recognition = new SpeechRecognition();
 recognition.interimResults = true;
-recognition.maxAlternatives = 1;
+recognition.continuous = true;
+recognition.lang = 'en-US';
 
+// Grab elements from the DOM
+let startRecognition = document.querySelector('#start');
+let stopRecognition = document.querySelector('#stop');
+let clearTranscript = document.querySelector('#clear');
+let playSpeech = document.querySelector('#play');
+let transcript = document.querySelector('#transcript');
 
-const startRecognition = () => {
-	console.log("Speech Recognition Started")
-	recognition.start();
+// Add event listeners
+startRecognition.addEventListener('click', handleStartRecognition);
+stopRecognition.addEventListener('click', handleStopRecognition);
+clearTranscript.addEventListener('click', handleClearTranscript);
+playSpeech.addEventListener('click', handlePlaySpeech);
+
+function handleStartRecognition(event) {
+  console.log('start speech recognition');
+
+  recognition.addEventListener('error', (event) => {
+    console.log('an error occurred');
+  });
+
+  recognition.addEventListener('result', (event) => {
+    const results = Array.from(event.results)
+      .map((item) => item[0].transcript)
+      .join('');
+
+    console.log(results);
+    transcript.textContent = results;
+  });
+
+  recognition.start();
 }
 
-document.getElementById('subBtn').addEventListener("click", startRecognition)
+function handleStopRecognition(event) {
+  console.log('stop speech recognition');
+  recognition.stop();
+}
+
+function handleClearTranscript(event) {
+  transcript.textContent = '';
+}
+
+function handlePlaySpeech(event) {
+  console.log('speech synthesis');
+
+  let utterance = new SpeechSynthesisUtterance(transcript.textContent);
+  synthesis.speak(utterance);
+}
